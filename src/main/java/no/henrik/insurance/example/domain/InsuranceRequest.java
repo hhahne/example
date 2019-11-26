ab1503a528b17081c21dca376c10fd9ff3babc01
hhahne <henrik.hahne@altran.com>
2019-11-26 22:28:11 +0100

    Added Lombok support to get rid of getters

--- src/main/java/no/henrik/insurance/example/domain/InsuranceRequest.java
+++ src/main/java/no/henrik/insurance/example/domain/InsuranceRequest.java
@@ -1,10 +1,13 @@
 package no.henrik.insurance.example.domain;
 
+import lombok.Getter;
+
 /**
  * Domain class for creating a simple (very simplified) insurance claim for a customer.
  *  Given more time, a proper class hierarchy could be set up and also mapped between Java and JSON.
  * It would also be a good idea to use something like a JSON Schema to define the API itself
  */
+@Getter
 public class InsuranceRequest {
     private String firstName;
     private String lastName;
@@ -19,25 +22,5 @@ public class InsuranceRequest {
         this.fnr = fnr;
         this.consentGiven = consentGiven;
         this.policyName = policyName;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public String getFnr() {
-        return fnr;
-    }
-
-    public boolean isConsentGiven() {
-        return consentGiven;
-    }
-
-    public String getPolicyName() {
-        return policyName;
     }
 }
--- src/main/java/no/henrik/insurance/example/domain/InsuranceResponse.java
+++ src/main/java/no/henrik/insurance/example/domain/InsuranceResponse.java
@@ -1,12 +1,12 @@
 package no.henrik.insurance.example.domain;
 
+import lombok.Getter;
+
+@Getter
 public class InsuranceResponse {
     private String customerNumber;
     private String policyNumber;
     private String status;
-
-    //Add a List of error message
-    //private String errorMessage;
 
     public InsuranceResponse(String customerNumber, String policyNumber, String status) {
         this.customerNumber = customerNumber;
@@ -14,18 +14,4 @@ public class InsuranceResponse {
         this.status = status;
     }
 
-
-    //Add Annotations
-
-    public String getCustomerNumber() {
-        return customerNumber;
-    }
-
-    public String getPolicyNumber() {
-        return policyNumber;
-    }
-
-    public String getStatus() {
-        return status;
-    }
 }
